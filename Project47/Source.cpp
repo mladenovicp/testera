b676260d348334c7f6581c636938389e1f5def21
Predrag Mladenovic <pex_boa@hotmail.com>
2018-03-17 15:31:35 +0100

    Dodat je samo  jedan cout ...

---
+++ Project47/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (;;)
+	{
+		cout << "Enter two integers: " << endl;
+		int num1 = 0, num2 = 0;
+		cin >> num1;
+		cin >> num2;
+
+		cout << "Do you wish to correct the numbes? (y/n): ";
+		char changeNumbers = '\0';
+		cin >> changeNumbers;
+
+		if (changeNumbers == 'y')
+			continue;
+
+		cout << num1 << " x " << num2 << " = " << num1 * num2 << endl;
+		cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+
+		cout << "Press x to exit or any other key to recalculate" << endl;
+		char userSelection = '\0';
+		cin >> userSelection;
+
+		if (userSelection == 'x')
+			break;
+	}
+
+	cout << "Goodbye!" << endl;
+
+	system("pause");
+	return 0;
+}
